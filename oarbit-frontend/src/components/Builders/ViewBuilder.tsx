6109f3889b6d604190321c6d54c49d4d9f9a547d
Oscar Laptop <oscarvlofthouse@gmail.com>
2026-05-15 20:26:21 +0100

    Matching past workouts
    Added matching past workouts viewer to enable progression view.
    next -> progress view

--- oarbit-frontend/src/components/Builders/ViewBuilder.tsx
+++ oarbit-frontend/src/components/Builders/ViewBuilder.tsx
@@ -1,7 +1,9 @@
-import { AssignedWorkout, IntervalTemplate, workoutTypeEnum } from '../../types/types';
-import { useNavigate } from 'react-router-dom'; 
+import { AssignedWorkout, assignedWorkoutStatus, CoachOverviewWorkout, IntervalTemplate, trainingTypeEnum, workoutTypeEnum } from '../../types/types';
+import { useNavigate } from 'react-router-dom';
 import './builders.css';
 import { secsToTimeString } from '../../utils/useTimeInput';
+import { useState, useEffect } from 'react';
+import { formatSplitTime } from '../../utils/timeFormats';
 
 interface ViewBuilerProps {
   workout: AssignedWorkout
@@ -10,6 +12,63 @@ interface ViewBuilerProps {
 export default function ViewBuilder(props: ViewBuilerProps) {
 
   const navigate = useNavigate();
+  const [viewMatched, setViewMatched] = useState(false);
+  const [matchingWorkouts, setMatchingWorkouts] = useState<CoachOverviewWorkout[]>([]);
+
+  useEffect(() => {
+    // Load Matching Workouts
+    setMatchingWorkouts([{
+      title: "2k",
+      date: new Date(),
+      location: "Sport Bu",
+      assignedCoach: "",
+      status: assignedWorkoutStatus.Completed,
+      trainingType: trainingTypeEnum.Land,
+      workoutType: workoutTypeEnum.singleDistance,
+      totalDistance: 2000,
+      totalTimeSecs: 381,
+      totalSplitSecs: 95.4,
+      avgHeartRate: 190,
+      avgStrokeRate: 33,
+      intervals: [{
+        intervalNumber: 1,
+        targetDistance: 2000,
+        targetTimeSecs: null,
+        completedDistance: 2000,
+        completedTimeSecs: 381,
+        completedSplitSecs: 95.4,
+        completedHeartRate: 190,
+        completedStrokeRate: 33,
+      }],
+      athleteName: "Oscar Lofthouse"
+    },
+    {
+      title: "2k",
+      date: new Date(),
+      location: "Sport Bu",
+      assignedCoach: "",
+      status: assignedWorkoutStatus.Completed,
+      trainingType: trainingTypeEnum.Land,
+      workoutType: workoutTypeEnum.singleDistance,
+      totalDistance: 2000,
+      totalTimeSecs: 420,
+      totalSplitSecs: 120,
+      avgHeartRate: 190,
+      avgStrokeRate: 33,
+      intervals: [{
+        intervalNumber: 1,
+        targetDistance: 2000,
+        targetTimeSecs: null,
+        completedDistance: 2000,
+        completedTimeSecs: 420,
+        completedSplitSecs: 120,
+        completedHeartRate: 190,
+        completedStrokeRate: 33,
+      }],
+      athleteName: "Oscar Lofthouse"
+    }
+    ])
+  }, [])
 
   function getTargetPlaceholderValue(interval: IntervalTemplate) {
     if (props.workout.workoutType == workoutTypeEnum.singleDistance ||
@@ -17,16 +76,16 @@ export default function ViewBuilder(props: ViewBuilerProps) {
       if (interval.targetDistance) return interval.targetDistance.toString()
     }
 
-    if (props.workout.workoutType == workoutTypeEnum.singleTime || 
+    if (props.workout.workoutType == workoutTypeEnum.singleTime ||
       props.workout.workoutType == workoutTypeEnum.intervalTime) {
-        if (interval.targetTimeSecs) return secsToTimeString(interval.targetTimeSecs);
-      }
+      if (interval.targetTimeSecs) return secsToTimeString(interval.targetTimeSecs);
+    }
 
     return "";
   }
 
   function handleLogSessionClick(workout: AssignedWorkout) {
-    navigate('/LogActivity', {state: {prefillWorkout: true, workout: workout}}); 
+    navigate('/LogActivity', { state: { prefillWorkout: true, workout: workout } });
   }
 
   return (
@@ -86,8 +145,55 @@ export default function ViewBuilder(props: ViewBuilerProps) {
                 </table>
               </div>
 
+              <div className="matched-past-container">
+                {viewMatched == false ?
+                  <>
+                    <div className="button-container">
+                      <button type="button" className="discard-btn" onClick={() => { setViewMatched(true) }}>View Past Matching Workouts</button>
+                    </div>
+                  </> : <>
+                    <div className="matched-past-content">
+                      <div className="title-container">
+                        <p>Matching Past Workouts</p>
+                      </div>
+                      <div className="table-wrap">
+                        <table>
+                          <thead>
+                            <th style={{ width: "10%" }}>Date</th>
+                            <th>{((
+                              props.workout.workoutType == workoutTypeEnum.singleDistance ||
+                              props.workout.workoutType == workoutTypeEnum.intervalDistance)
+                              ? "Time (mins)" : "Distance (m)")}
+                            </th>
+                            <th>Split</th>
+                            <th>SPM</th>
+                            <th>HR</th>
+                          </thead>
+                          <tbody>
+                            {matchingWorkouts.map((matched) => (
+                              <tr>
+                                <td>{matched.date.getDate()}/{matched.date.getMonth()}</td>
+                                {/* Determine Main Metric here */
+                                  (
+                                    props.workout.workoutType == workoutTypeEnum.singleDistance ||
+                                    props.workout.workoutType == workoutTypeEnum.intervalDistance) ?
+                                    <td>{secsToTimeString(matched.totalTimeSecs)}</td> :
+                                    <td>{matched.totalDistance}</td>
+                                } 
+                                <td>{formatSplitTime(matched.totalSplitSecs)}</td>
+                                <td>{matched.avgStrokeRate ?? "-"}</td>
+                                <td>{matched.avgHeartRate ?? "-"}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </>}
+              </div>
+
               <div className="log-button-container">
-                <button type="button" className="view-builder-log-btn" onClick={() => {handleLogSessionClick(props.workout)}}>Log Session</button>
+                <button type="button" className="view-builder-log-btn" onClick={() => { handleLogSessionClick(props.workout) }}>Log Session</button>
               </div>
             </div>
           </form>
